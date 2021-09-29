d563bfc17073315efd5966dbba9b7fde06f216bf
Maliha Tunzira <mtunjira@yahoo.com>
2021-09-29 10:20:43 +0600

    second commit

--- ifstatement.php
+++ ifstatement.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php-Home</title>
+    <title>If Statemnt</title>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <h2>here are some links to follow rules</h2>
+<h2>here are some links to follow rules</h2>
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="ifstatement.php">If Statement in php</a></li>
@@ -16,8 +15,16 @@
         <li><a href=""></a></li>
         <li><a href=""></a></li>
     </ul>
+    <h3 style="color: blueviolet;">here is the example output</h3>
+
     <?php
-        echo 'php codes';
+        $grade = 20;
+        
+        if($grade >= 50){
+            echo "You Have Passed the exam by ".$grade;
+        }else{
+            echo "<h5 style='color: red'> You Have failed </h5>";
+        }
     ?>
 </body>
 </html>
--- switch.php
+++ switch.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php-Home</title>
+    <title>switch Statement in PHP</title>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <h2>here are some links to follow rules</h2>
+<h2>here are some links to follow rules</h2>
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="ifstatement.php">If Statement in php</a></li>
@@ -16,8 +15,6 @@
         <li><a href=""></a></li>
         <li><a href=""></a></li>
     </ul>
-    <?php
-        echo 'php codes';
-    ?>
+    
 </body>
 </html>
